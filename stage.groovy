f5a2f2a0d65ca5e81fce52d8c7c8d5ea04d84afc
elvis-ngeh <104249773+elvis-ngeh@users.noreply.github.com>
2023-01-16 20:46:00 -0500

    Update stage.groovy

--- stage.groovy
+++ stage.groovy
@@ -1,12 +1,11 @@
-def call (string  stageName){
-    if('"${stageName}" == "Build"')
+def call (String  stageName){
+    if ("${stageName}" == "Build"){
+    sh "mvn clean package"
+    }
+    else if ("${stageName}" == "Sonarqube Analyses"){
+    sh "mvn sonar:sonar"
+    }
+    else if ("${stageName}" == "Upload Artifacts"){
+    sh "mvn deploy"
+    }
 }
-sh "mvn clean package"
-{
- else if ('"${stageName}" == "SonarqubeReport"')
-}
-sh " mvn sonar:sonar"
-{
- else if ('"${stageName}" == "Upload to Nexus"')
-}
-sh "mvn deploy"
